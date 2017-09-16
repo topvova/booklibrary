c79b0d1240f8a8fb3c371fafc463dab615914e02
topvova <topvova@gmail.com>
2017-09-16 21:08:34 +0300

    Update repositories

--- app/Repositories/BookRepository.php
+++ app/Repositories/BookRepository.php
@@ -2,10 +2,10 @@
 
 namespace App\Repositories;
 
-
+use App\Entities\Author;
 use App\Entities\Book;
 
-class BookRepository
+class BookRepository extends BaseRepository
 {
     /**
      * Specify Model class name.
@@ -15,5 +15,17 @@ class BookRepository
     protected function model()
     {
         return Book::class;
+    }
+
+    /**
+     * Show latest books by author
+     *
+     * @param $author
+     * @return mixed
+     */
+    public function booksByAuthor(Author $author) {
+        $books = $author->books();
+
+        return $books->orderBy('id', 'desc');
     }
 }
