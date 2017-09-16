e884fb0c2716a19dc64763d5b02b96a65ce79622
topvova <topvova@gmail.com>
2017-09-16 11:54:06 +0300

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,29 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+/*
+|--------------------------------------------------------------------------
+| Main Routes
+|--------------------------------------------------------------------------
+*/
+Route::get('/', 'BookController@index');
+Route::get('/books/{book}', 'BookController@show');
+Route::get('/authors/{author}', 'AuthorController@show');
+
+/*
+|--------------------------------------------------------------------------
+| Admin control panel routes
+|--------------------------------------------------------------------------
+*/
+Route::get('/library/books', 'Library\BookController@index');                                //show
+Route::post('/library/books', 'Library\BookController@store');                               //create
+Route::get('/library/books/edit/{book}', 'Library\BookController@edit');                     //show edit
+Route::patch('/library/books/{book}', 'Library\BookController@update');                      //update
+Route::delete('/library/books/{book}', 'Library\BookController@delete');                     // delete
+
+Route::get('/library/authors', 'Library\AuthorController@index');                            //show
+Route::post('/library/authors', 'Library\AuthorController@store');                           //create
+Route::get('/library/authors/edit/{author}', 'Library\AuthorController@edit');               //show edit
+Route::patch('/library/authors/{author}', 'Library\AuthorController@update');                //update
+Route::delete('/library/authors/{author}', 'Library\AuthorController@delete');               //delete
