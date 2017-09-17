9b3e410958ca40d079f9dc8f6e6d1ab8d76521e1
topvova <topvova@gmail.com>
2017-09-17 22:16:21 +0300

    Create admin BookController

---
+++ app/Http/Controllers/Admin/BookController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\AuthorRepository;
+use App\Repositories\BookRepository;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display all authors and books.
+     *
+     * @param BookRepository $bookRepository
+     * @param AuthorRepository $authorRepository
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index(BookRepository $bookRepository, AuthorRepository $authorRepository)
+    {
+        $books = $bookRepository->paginate(5);
+        $authors = $authorRepository->all();
+
+        return view('admin.book.list', [
+            'books' => $books,
+            'authors' => $authors
+        ]);
+    }
+    /**
+     * Store a new book.
+     *
+     * @param Request $request
+     * @param BookRepository $bookRepository
+     * @return $this|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request, BookRepository $bookRepository)
+    {
+        $this->validate($request, [
+            'title'=> 'required|max:255|min:5',
+            'description'=>'required|min:50',
+            'image'=>'required|mimes:jpeg,bmp,png',
+            'authors'=>'required'
+        ]);
+
+        if (!$request->file('image')->isValid()) {
+            return redirect('/admin/books')
+                ->withErrors('Завантажте зображення');
+        }
+
+        $result = $bookRepository->create($request);
+
+        if ($result != false) {
+            return redirect('/admin/books');
+        } else {
+            return redirect('/admin/books')
+                ->withErrors('Не вдалось зберегти');
+        }
+    }
+    /**
+     * Show the form for editing the book.
+     *
+     * @param BookRepository $bookRepository
+     * @param $bookId
+     * @param AuthorRepository $authorRepository
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(BookRepository $bookRepository, $bookId, AuthorRepository $authorRepository)
+    {
+        $book = $bookRepository->find($bookId);
+        $authors = $authorRepository->all();
+
+        return view('admin.book.edit', [
+            'book' => $book,
+            'authors' => $authors
+        ]);
+    }
+
+    /**
+     * Update the book.
+     *
+     * @param Request $request
+     * @param BookRepository $bookRepository
+     * @param $bookId
+     * @return $this|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function update(Request $request, BookRepository $bookRepository, $bookId)
+    {
+        $this->validate($request, [
+            'title'=> 'required|max:255|min:5',
+            'description'=>'required|min:50',
+            'image'=>'mimes:jpeg,bmp,png',
+            'authors'=>'required'
+        ]);
+
+        $result = $bookRepository->update($request->all(), $bookId);
+
+        if ($result != false) {
+            return redirect('/admin/books');
+        } else {
+            return redirect('/admin/books')
+                ->withErrors('Не вдалось зберегти');
+        }
+    }
+
+    /**
+     * Delete the book.
+     *
+     * @param BookRepository $bookRepository
+     * @param $bookId
+     * @return $this|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function delete(BookRepository $bookRepository, $bookId)
+    {
+        $result = $bookRepository->delete($bookId);
+
+        if ($result != false) {
+            return redirect('/admin/books');
+        } else {
+            return redirect('/admin/books')
+                ->withErrors('Не вдалось видалити');
+        }
+    }
+}
