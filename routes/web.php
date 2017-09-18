8322ea24896b764fa7c412e0de0f6006f3081134
topvova <topvova@gmail.com>
2017-09-18 21:03:50 +0300

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -26,14 +26,8 @@ Route::get('/authors/{author}', 'AuthorController@show');
 | Admin control panel routes
 |--------------------------------------------------------------------------
 */
-//Route::get('/library/books', 'Library\BookController@index');                                //show
-//Route::post('/library/books', 'Library\BookController@store');                               //create
-//Route::get('/library/books/edit/{book}', 'Library\BookController@edit');                     //show edit
-//Route::patch('/library/books/{book}', 'Library\BookController@update');                      //update
-//Route::delete('/library/books/{book}', 'Library\BookController@delete');                     // delete
-//
-//Route::get('/library/authors', 'Library\AuthorController@index');                            //show
-//Route::post('/library/authors', 'Library\AuthorController@store');                           //create
-//Route::get('/library/authors/edit/{author}', 'Library\AuthorController@edit');               //show edit
-//Route::patch('/library/authors/{author}', 'Library\AuthorController@update');                //update
-//Route::delete('/library/authors/{author}', 'Library\AuthorController@delete');               //delete
+Route::get('/admin/books', 'Admin\BookController@index');
+Route::post('/admin/books', 'Admin\BookController@store');
+Route::get('/admin/books/edit/{book}', 'Admin\BookController@edit');
+Route::patch('/admin/books/{book}', 'Admin\BookController@update');
+Route::delete('/admin/books/{book}', 'Admin\BookController@delete');
